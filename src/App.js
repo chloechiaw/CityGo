81f51d767568e8b4ad323347c2b2aff11f2fd486
Abinaya Dinesh <abinayadinesh1@gmail.com>
2023-02-18 22:04:34 -0800

    got streetview into side menu

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import * as React from "react";
 import { Prompt } from "./prompt.js";
 import { useState, useEffect, useMemo } from "react";
 import { v4 } from "uuid";
-import StreetView from './StreetView'
 import MapView from "./MapView";
 import SideMenu from "./SideMenu";
-import { LoadScript } from "@react-google-maps/api";
-
 
 
 // add an id to the map and the popup. if the (conditions) matches then the popup shows up.
 
 export default function App() {
-  const lib = ["places"];
-  const key = "AIzaSyAZgZEKZ6djLHvWI9g5qkQGdDbInfSJ0nE";
-
   const [locations, setLocations] = useState([]);
   const [displaySideMenu, setDisplaySideMenu] = useState({
     Title: "No area selected",
@@ -61,14 +55,12 @@ export default function App() {
 
   return (
     <div>
-      <LoadScript googleMapsApiKey={key} libraries={lib}>
-        <StreetView lat={37.7857} lng={122.4011}/>
-      </LoadScript>
+      <MapView setDisplaySideMenu={setDisplaySideMenu} locations={locations}/>
       <SideMenu
         title={displaySideMenu.Title}
         description={displaySideMenu.description}
         suggestions={displaySideMenu.suggestions}
-      ></SideMenu>
+      />
     </div>
   );
 }
